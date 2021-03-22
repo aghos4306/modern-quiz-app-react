7c08f300a963646f518b461e13d2300e616f45f6
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-22 12:59:42 +0200

    context setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
+function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext()
+  if(waiting) {
+    return <SetupForm />
+  } 
+  if(loading) {
+    return <Loading />
+  }
+  return (
+    <main>Quiz Application</main>
+  )
+}
+
+export default App
---
+++ src/context.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useState, useContext, useEffect } from 'react'
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+}
+
+const API_ENDPOINT = 'https://opentdb.com/api.php?'
+
+const url = ''
+
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return <AppContext.Provider 
+    value={{waiting, 
+            loading, 
+            questions, 
+            index, 
+            correct, 
+            error, 
+            isModalOpen
+          }}>
+            {children}
+          </AppContext.Provider>
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
