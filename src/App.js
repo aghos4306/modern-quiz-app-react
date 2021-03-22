bac4766e9e29d0431788e02bd549649831bd04ff
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-22 22:59:45 +0200

    display questions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,30 @@ function App() {
   if(loading) {
     return <Loading />
   }
+  //console.log(questions)
+  const {question, incorrect_answers, correct_answer} = questions[index]
+  const answers = [...incorrect_answers, correct_answer]
+
   return (
-    <main>Quiz Application</main>
+    <main>
+      {/* <Modal />  */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{__html: question}} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button key={index} className="answer-btn" dangerouslySetInnerHTML={{__html: answer}} />
+              )
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
   )
 }
 
