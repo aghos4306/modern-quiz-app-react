7057467b6bc516b40bbba3aaf4c2f58419679cc0
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-24 10:52:36 +0200

    random index on answers to quiz

--- src/App.js
+++ src/App.js
@@ -14,7 +14,15 @@ function App() {
   }
   //console.log(questions)
   const {question, incorrect_answers, correct_answer} = questions[index]
-  const answers = [...incorrect_answers, correct_answer]
+  //const answers = [...incorrect_answers, correct_answer]
+  let answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random() * 4)
+  if(tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
 
   return (
     <main>
